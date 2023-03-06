a2aa98d1f99c9adb53accb47d6829abfae47021a
PHCorra <ph.corra@gmail.com>
2023-03-06 10:35:08 -0300

    chore: Initial setup of the project, with docker compose file to create the mysql container, tested and connected

---
+++ src/test/java/com/br/whatsCodePaymentMicroservice/WhatsCodePaymentMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.whatsCodePaymentMicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhatsCodePaymentMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
