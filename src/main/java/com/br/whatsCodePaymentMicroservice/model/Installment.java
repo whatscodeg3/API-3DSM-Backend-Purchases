96e6df264b4f27d0d204b53c78134edaaac1f6d6
PHCorra <ph.corra@gmail.com>
2023-03-10 15:18:38 -0300

    feat: Creating folders for the architechure of the project, creating Purchase and Installment models and connecting to the mysql container db

---
+++ src/main/java/com/br/whatsCodePaymentMicroservice/model/Installment.java
@@ -0,0 +1,24 @@
+package com.br.whatsCodePaymentMicroservice.model;
+
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Data
+@Entity
+@Table(name = "installment")
+public class Installment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="id_Installment")
+    private Long id;
+
+    @ManyToMany(mappedBy = "installment")
+    private Set<Purchase> purchases = new HashSet<>();
+
+}
+
---
+++ src/main/java/com/br/whatsCodePaymentMicroservice/model/Purchase.java
@@ -0,0 +1,31 @@
+package com.br.whatsCodePaymentMicroservice.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.util.Date;
+import java.util.HashSet;
+import java.util.Set;
+
+@Data
+@Entity
+@Table(name = "purchase")
+public class Purchase {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="id_purchase")
+    private Long id;
+
+    @Column(name="payment_value")
+    private Double paymentValue;
+
+    @Column(name="purchase_date")
+    private Date purchaseDate;
+
+    @ManyToMany
+    @JoinTable(name = "purchase_installment", joinColumns = {@JoinColumn(name= "id_purchase")}, inverseJoinColumns =
+            {@JoinColumn(name = "id_installment")})
+    private Set<Installment> installment = new HashSet<>();
+
+}
