dbad90521a15921c7c882d8e7100062afe4e33ce
PHCorra <ph.corra@gmail.com>
2023-03-15 10:34:22 -0300

    feat: creating method to find by name

--- src/main/java/com/br/whatsCodePaymentMicroservice/repository/PurchaseRepository.java
+++ src/main/java/com/br/whatsCodePaymentMicroservice/repository/PurchaseRepository.java
@@ -2,6 +2,11 @@ package com.br.whatsCodePaymentMicroservice.repository;
 
 import com.br.whatsCodePaymentMicroservice.model.Purchase;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface PurchaseRepository extends JpaRepository<Purchase, Long> {
+    @Query("SELECT Client.fullName from Client WHERE fullName=:fullname")
+    Purchase findByName(@Param("fullName")String fullName);
+
 }
