caf7bc815e5540d846d1f2a2b7ada2626602f368
Lucas Medici <lucasrpmedici@gmail.com>
2023-05-14 18:56:49 -0300

    importando annotation get no authcontroller

--- src/main/java/com/br/whatsCodePaymentMicroservice/controller/AuthController.java
+++ src/main/java/com/br/whatsCodePaymentMicroservice/controller/AuthController.java
@@ -8,6 +8,7 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
